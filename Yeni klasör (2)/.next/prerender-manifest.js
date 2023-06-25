49b7ff751859c00f9575f2442e625614bb95af40
whatcost99 <isimdegistir21@gmail.com>
2023-06-25 16:43:06 +0300

    hadi baba yapabilirsinnnn

---
+++ Yeni klasör (2)/.next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"4a3f2b8d094d3774d8f853019e6a19e0\",\"previewModeSigningKey\":\"bd4df3f038b28bbb134ed613688d4f7dcb0c6beb08aff2b8d77fd3cc2ed6c86a\",\"previewModeEncryptionKey\":\"ad9be372f8584446efdd4d934bac60ace31e783f863f1e5b8ec5b6215eb75e67\"},\"notFoundRoutes\":[]}"
